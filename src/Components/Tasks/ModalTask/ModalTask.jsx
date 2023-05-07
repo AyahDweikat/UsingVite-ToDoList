47ee79afa0cc29b78dac88c24b7046b16d0372bc
AyahDweikat <dweikatayah99@gmail.com>
2023-05-07 20:19:55 +0300

    display tasks and modal to click for deleting tasks

---
+++ src/Components/Tasks/ModalTask/ModalTask.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import styles from './modalTask.module.css'
+function ModalTask({closeHandle, deleteTask, id}) {
+  return (
+    <div>
+      <div className={styles.modal}>
+        <h3>Delete Task</h3>
+        <p>Are you sure you want to delete this task?</p>
+        <span>
+          This is a permenant deletion, you won&apos;t be able to undo it
+        </span>
+        <div className={styles.modalBtns}>
+          <button
+            type="button"
+            data-bs-dismiss="modal"
+            onClick={()=>closeHandle()}
+          >
+            Cancel
+          </button>
+          <button type="button" id="delBtn" 
+          onClick={() => {
+            deleteTask(id);
+            closeHandle();
+          }} 
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ModalTask;
---
+++ src/Components/Tasks/Tasks.jsx
@@ -0,0 +1,122 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ModalTask from "./ModalTask/ModalTask";
+import { useState } from "react";
+import styles from "./tasks.module.css";
+import Modal from "react-overlays/Modal";
+
+function Tasks({ tasks, changeState, deleteTask, editTask }) {
+  const [showModal, setShowModal] = useState(false);
+  const [flagEditing, setFlagEditing] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const renderBackdrop = (props) => <div className="backdrop" {...props} />;
+  var closeHandle = () => {
+    setShowModal(false);
+  };
+
+  function turnToEdit() {
+    setFlagEditing(true);
+  }
+  function turnToTask() {
+    setFlagEditing(false);
+  }
+  function InputHandle(e) {
+    setInputValue(e.target.value);
+  }
+  function submitInput(id) {
+    editTask(id, inputValue);
+    setInputValue("");
+    turnToTask();
+  }
+  return (
+    <section className={styles.tasks}>
+      <ul id="taskDisplay" className={styles.taskList}>
+        {tasks.map((item) => {
+          return (
+            <li
+              key={item.id}
+              className={item.doingState ? styles.doneTask : styles.undoneTask}
+            >
+              <div>
+                <button
+                  className={styles.btnDoneState}
+                  onClick={() => changeState(item.id, tasks)}
+                >
+                  <i
+                    className={
+                      item.doingState
+                        ? `fa-solid fa-circle-check ${styles.doneBtn}`
+                        : `fa-regular fa-circle ${styles.undoneBtn}`
+                    }
+                    style={{ color: "#14bba6" }}
+                  ></i>
+                </button>
+              </div>
+              <div key={item.id}>
+                {flagEditing ? (
+                  <input
+                    key={item.id}
+                    className={styles.editInpt}
+                    onBlur={() => submitInput(item.id)}
+                    onChange={(e) => {
+                      InputHandle(e);
+                    }}
+                    value={inputValue}
+                  />
+                ) : (
+                  <p
+                    key={item.id}
+                    onDoubleClick={() => {
+                      setInputValue(item.task);
+                      turnToEdit();
+                    }}
+                  >
+                    {item.task}
+                  </p>
+                )}
+                <span>{item.assignee}</span>
+              </div>
+              <div className={styles.delEditBtn}>
+                <button
+                  className={styles.deleteBtn}
+                  onClick={() => {
+                    setShowModal(true);
+                  }}
+                  // onClick={() => deleteTask(item.id, tasks)}
+                >
+                  <i
+                    className="fa-solid fa-trash"
+                    style={{ color: "#FF0000" }}
+                  ></i>
+                </button>
+                <button
+                  className={styles.btnEdit}
+                  onClick={turnToEdit}
+                  disabled={item.doingState}
+                >
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+              </div>
+              <Modal
+                className={styles.modal}
+                show={showModal}
+                onHide={closeHandle}
+                renderBackdrop={renderBackdrop}
+              >
+                <ModalTask
+                  closeHandle={closeHandle}
+                  deleteTask={deleteTask}
+                  tasks={tasks}
+                  id={item.id}
+                />
+              </Modal>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+export default Tasks;
